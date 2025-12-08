721309193030182fb0126c6f4b618a43659a87ca
nicolasinohue <nicolasinohue@gmai.com>
2025-12-08 05:24:37 -0300

    Implementando a camada de consultas - Controller-DTOs-Repository-EntityJPA-Migration

---
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,23 @@
+package med.voll.api.controller;
+
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import med.voll.api.domain.medico.DadosDetalhamentoMedico;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("consultas")
+public class ConsultaController {
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        System.out.println(dados);
+        return ResponseEntity.ok().body(new DadosDetalhamentoConsulta(null, null, null, null));
+    }
+
+}
